bb641e6d31a69e12fb420e2d2b8ecd3886565e4f
jin <s3736719@student.rmit.edu.au>
2020-07-21 12:12:58 +1000

    Fix responsiveness bug

---
+++ build/precache-manifest.47c40d32fcaa282523462412bbb8a667.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf24d7da9f77e22e1d6aaf10539744f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b202f92b563803b97f1c",
+    "url": "/react-portfolio/static/css/main.828bf54e.chunk.css"
+  },
+  {
+    "revision": "82ae99bbb3e0e22547f4",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.11283df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b202f92b563803b97f1c",
+    "url": "/react-portfolio/static/js/main.b4ad3783.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "4e240d7012724096eb9fa50fb659b7b3",
+    "url": "/react-portfolio/static/media/128px-PostgreSQL.4e240d70.png"
+  },
+  {
+    "revision": "2a38ae5c7d3cb836be92aeab5a1380ac",
+    "url": "/react-portfolio/static/media/profile.2a38ae5c.jpg"
+  },
+  {
+    "revision": "e7d8c1960c437bd82682356bcb4160d4",
+    "url": "/react-portfolio/static/media/project.e7d8c196.jpg"
+  }
+]);
